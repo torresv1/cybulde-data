bd68a7178073567a5f7156fa5d9385bb6e8917ec
torresv1 <torres_vladimir@hotmail.com>
2025-11-17 20:23:31 -0500

    Added functionality DVC storage

--- cybulde/utils/data_utils.py
+++ cybulde/utils/data_utils.py
@@ -8,13 +8,26 @@ DATA_UTILS_LOGGER = get_logger(Path(__file__).name)
 def is_dvc_initialized() -> bool:
     return (Path.cwd() / ".dvc").exists()
 
-def initialize_dvc():
+def initialize_dvc() -> None:
     if is_dvc_initialized():
         DATA_UTILS_LOGGER.info("DVC is already initialized.")
         return
+    
     DATA_UTILS_LOGGER.info("Initializing DVC...")
     run_shell_command("dvc init")
     run_shell_command("dvc config core.analytics false")
     run_shell_command("dvc config core.auto_stages true")
     run_shell_command("git add .dvc")
     run_shell_command("git commit -nm 'Initialized DVC'")
+
+def initialize_dvc_storage(remote_name: str, remote_url: str)-> None:
+    if not run_shell_command("dvc remote list"):
+        DATA_UTILS_LOGGER.info("Initializing DVC storage...")
+        run_shell_command(f"dvc remote add -d {remote_name} {remote_url}")
+        run_shell_command("git add .dvc/config")
+        run_shell_command(f"git commit -nm 'Configured remote storage at: {remote_name}'")
+    else:
+        DATA_UTILS_LOGGER.info("DVC remote was already initialized.")
+        return
+        
+
--- cybulde/version-data.py
+++ cybulde/version-data.py
@@ -2,13 +2,14 @@ from pathlib import Path
 
 from cybulde.config_schemas.config_schema import Config
 from cybulde.utils.config_utils import get_config
-from cybulde.utils.data_utils import initialize_dvc
+from cybulde.utils.data_utils import initialize_dvc, initialize_dvc_storage
 from cybulde.utils.utils import get_logger
 
 
 @get_config(config_path="../configs", config_name="config")
 def version_data(config: Config) -> None:
     initialize_dvc()
+    initialize_dvc_storage(config.dvc_remote_name, config.dvc_remote_url)
 
 
 if __name__ == "__main__":
