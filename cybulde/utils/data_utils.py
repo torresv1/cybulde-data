7ee8c2892f9fdc9cb63d464f39900d34317121ea
torresv1 <torres_vladimir@hotmail.com>
2025-11-23 12:19:04 -0500

    Refactor DVC initialization and storage configuration for clarity and consistency

--- cybulde/utils/data_utils.py
+++ cybulde/utils/data_utils.py
@@ -6,14 +6,16 @@ from cybulde.utils.utils import get_logger, run_shell_command
 
 DATA_UTILS_LOGGER = get_logger(Path(__file__).name)
 
+
 def is_dvc_initialized() -> bool:
     return (Path.cwd() / ".dvc").exists()
+
 
 def initialize_dvc() -> None:
     if is_dvc_initialized():
         DATA_UTILS_LOGGER.info("DVC is already initialized.")
         return
-    
+
     DATA_UTILS_LOGGER.info("Initializing DVC...")
     run_shell_command("dvc init")
     run_shell_command("dvc config core.analytics false")
@@ -21,31 +23,47 @@ def initialize_dvc() -> None:
     run_shell_command("git add .dvc")
     run_shell_command("git commit -nm 'Initialized DVC'")
 
-def initialize_dvc_storage(remote_name: str, remote_url: str)-> None:
+
+def initialize_dvc_storage(remote_name: str, remote_url: str) -> None:
     if not run_shell_command("dvc remote list"):
         DATA_UTILS_LOGGER.info("Initializing DVC storage...")
         run_shell_command(f"dvc remote add -d {remote_name} {remote_url}")
         run_shell_command("git add .dvc/config")
-        run_shell_command(f"git commit -nm 'Configured remote storage at: {remote_name}'")
+        run_shell_command(
+            f"git commit -nm 'Configured remote storage at: {remote_url}'"
+        )
     else:
         DATA_UTILS_LOGGER.info("DVC remote was already initialized.")
         return
 
+
 def commit_to_dvc(dvc_raw_data_folder: str, dvc_remote_name: str) -> None:
-    current_version = run_shell_command("git tag --list | sort -t v -k 2 -g | tail 1 |sed 's/v//'").strip()
+    current_version = run_shell_command(
+        "git tag --list | sort -t v -k 2 -g | tail 1 |sed 's/v//'"
+    ).strip()
     if not current_version:
         current_version = "0"
     next_version = f"v{int(current_version) + 1}"
     run_shell_command(f"dvc add {dvc_raw_data_folder}")
-    run_shell_command(f"git add .")
-    run_shell_command(f"git commit -nm 'Updated version of the data from v{current_version} to {next_version}'")
-    run_shell_command(f"git tag -a {next_version} -m 'Data version {next_version}'")
-    run_shell_command(f"dvc push {dvc_raw_data_folder}.dvc --remote {dvc_remote_name}")
+    run_shell_command("git add .")
+    run_shell_command(
+        f"git commit -nm 'Updated version of the data from "
+        f"v{current_version} to {next_version}'"
+    )
+    run_shell_command(
+        f"git tag -a {next_version} -m 'Data version {next_version}'"
+    )
+    run_shell_command(
+        f"dvc push {dvc_raw_data_folder}.dvc "
+        f"--remote {dvc_remote_name}"
+    )
     run_shell_command("git push --follow-tags")
-    run_shell_command(f"dvc push -f --tags")
+    run_shell_command("dvc push -f --tags")
 
 
-def make_new_data_version(dvc_raw_data_folder: str, dvc_remote_name: str) -> None:
+def make_new_data_version(
+    dvc_raw_data_folder: str, dvc_remote_name: str
+) -> None:
     try:
         status = run_shell_command(f"dvc status {dvc_raw_data_folder}.dvc")
         if status == "Data and pipelines are up to date.\n":
